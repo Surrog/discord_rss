3f8fdb09586b6b6a753548ad7c790627905a7fa9
Surogate <errata.dark@gmail.com>
2020-05-01 21:47:44 +0200

    cleanup messages sent to discord, /last news nows bring only the last first news, log file now limited to 5 MB

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ import dateutil.parser
 import feedparser
 import asyncio
 import logging
+from logging.handlers import RotatingFileHandler
 import sys
 from bs4 import BeautifulSoup
 from time import mktime
@@ -38,10 +39,20 @@ if (os.path.isfile(CONFIGURATION_PATH)):
 with open(CONFIGURATION_PATH, "w") as f:
     json.dump(configuration, f)
 
-logging.basicConfig(filename=LOGGING_PATH, level=logging.INFO, filemode='w')
+log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(lineno)d) %(message)s')
+
+my_handler = RotatingFileHandler(LOGGING_PATH, mode='w', maxBytes=5*1024*1024, 
+                                 backupCount=2, encoding=None, delay=0)
+my_handler.setFormatter(log_formatter)
+my_handler.setLevel(logging.INFO)
+
+app_log = logging.getLogger('root')
+app_log.setLevel(logging.INFO)
+
+app_log.addHandler(my_handler)
 
 def exception_handler(type, value, tb):
-    logging.exception("Uncaught exception: {0}".format(str(value)))
+    app_log.exception("Uncaught exception: {0}".format(str(value)))
 
 sys.excepthook = exception_handler
 threading.excepthook = exception_handler
@@ -65,24 +76,62 @@ async def fetch_initial_link():
                 if item["link"] not in configuration["link_done"][url]:
                     configuration["link_done"][url].append(item["link"])
 
-def parse_date(item):
-    if "date_parsed" in item:
-        return datetime.datetime.fromtimestamp(mktime(item["date_parsed"]))
-    if "published_parsed" in item:
-        return datetime.datetime.fromtimestamp(mktime(item["published_parsed"]))
-    if "date" in item:
-        return dateutil.parser.parse(item["date"])
-    if "published" in item:
-        return dateutil.parser.parse(item["published"])
-    return None
+def format_message(item):
+    message = "__**" + item["title"] + "**__\n"
+    message += cleanup_summary(item["summary"]) + '\n'
+    limit = 1900 - len(item["link"]) - len("...")
 
-async def pull_news(ignore_previous=False, ctx=None):
+    if len(message) > limit:
+        message = message[:limit] + "...";
+
+    if item["link"] not in message:
+        message += item["link"]
+    return message
+
+async def pull_news(ctx=None):
     global configuration
-    logging.info("pulling news !")
-    date_now = datetime.datetime.now()
-    previous_run = dateutil.parser.parse(configuration["last_run"])
+    app_log.info("pulling news !")
 
-    messages = ["Today I bring _SHOCKING NEWS_ to you !"]
+    messages = ["Today I bring _SHOCKING NEWS_  to you !"]
+    url_errored = []
+
+    if ctx is None and "chan_target_id" in configuration:
+        ctx =  bot.get_channel(configuration["chan_target_id"])
+
+    for url in configuration["url_list"]:
+        app_log.info("pulling from " + url)
+        feed = feedparser.parse(url)
+        if feed["bozo"] == 0:
+            message = ""
+            for item in feed["items"]:
+                if item["link"] not in configuration["link_done"][url]:
+                    message = format_message(item)
+                    if len(message):
+                        messages.append(message)            
+                    configuration["link_done"][url].append(item["link"])
+
+            if len(configuration["link_done"][url]) > 20:
+                configuration["link_done"][url] = configuration["link_done"][url][-19:]           
+        else:
+            app_log.warning("feedparser failed to parse {0} ".format(url))
+            url_errored.append(url)
+
+    if ctx is not None:
+        if len(messages) > 1:
+            for message in messages:
+                await ctx.send(message)
+        
+        if len(url_errored) > 0:
+                await ctx.send("Sorry these url(s) failed: {0}".format(str(url_errored)))
+
+    configuration["last_run"] = str(datetime.datetime.now())
+    with open(CONFIGURATION_PATH, "w") as f:
+        json.dump(configuration, f)
+
+async def pull_last_news(ctx=None):
+    app_log.info("pulling last news !")
+
+    messages = ["Today I bring _SHOCKING NEWS_  to you !"]
     url_errored = []
 
     local_ctx = None
@@ -92,59 +141,31 @@ async def pull_news(ignore_previous=False, ctx=None):
         local_ctx = ctx
 
     for url in configuration["url_list"]:
-        logging.info("pulling from " + url)
+        app_log.info("pulling from " + url)
         feed = feedparser.parse(url)
         if feed["bozo"] == 0:
             if url not in configuration["link_done"]:
                 configuration["link_done"][url] = []
             
             message = ""
-            for item in feed["items"]:
-                date = parse_date(item) 
-
-                if (news_before_date(previous_run, date) and item["link"] not in configuration["link_done"][url]) or ignore_previous:
-                    new_message = "__**" + item["title"] + "**__\n"
-                    new_message += cleanup_summary(item["summary"]) + '\n'
-                    limit = 1900 - len(item["link"]) - len("...")
-    
-                    if len(new_message) > limit:
-                        new_message = new_message[:limit] + "...";
-    
-                    if item["link"] not in new_message:
-                        new_message += item["link"]
-    
-                    if len(message) + len(new_message) + 1 > 2000:
-                        messages.append(message)
-                        message = new_message
-                    else:
-                        message += '\n' + new_message
-                
-                    if not ignore_previous and item["link"] not in configuration["link_done"][url]:
-                        configuration["link_done"][url].append(item["link"])
-
+            if len(feed["items"]) > 0:
+                message = format_message(feed["items"][0])
+            
             if len(message):
                 messages.append(message)
-
-            if len(configuration["link_done"][url]) > 20:
-                configuration["link_done"][url] = configuration["link_done"][url][-19:]           
         else:
-            logging.warning("feedparser failed to parse {0} ".format(url))
+            app_log.warning("feedparser failed to parse {0} ".format(url))
             url_errored.append(url)
 
     if local_ctx is not None:
         if len(messages) > 1:
             for message in messages:
                 await local_ctx.send(message)
-        else:
+        elif ctx is not None:
             await local_ctx.send("No news to post ! :(")
         
         if len(url_errored) > 0:
                 await local_ctx.send("Sorry these url(s) failed: {0}".format(str(url_errored)))
-
-    if ctx is None:
-        configuration["last_run"] = str(date_now)
-        with open(CONFIGURATION_PATH, "w") as f:
-            json.dump(configuration, f)
 
 timer = None
 async def pull_news_at_interval():
@@ -154,23 +175,23 @@ async def pull_news_at_interval():
 
 @bot.event
 async def on_ready():
-    logging.info(f'{bot.user.name} is now connected to Discord')
+    app_log.info(f'{bot.user.name} is now connected to Discord')
     global configuration
     for guild in bot.guilds:
         for channel in guild.text_channels:
             if channel.name == CHANNEL_TARGET:
-                logging.info("found channel: " + channel.name)
+                app_log.info("found channel: " + channel.name)
                 configuration["chan_target_id"] = channel.id
     if "chan_target_id" not in configuration:
-        logging.warning("can't find channel: " + CHANNEL_TARGET)
+        app_log.warning("can't find channel: " + CHANNEL_TARGET)
     await fetch_initial_link()
     await pull_news_at_interval()
 
-@bot.command(name='last_news', help='Fetch and post last news')
+@bot.command(name='last_news', help='Fetch and post lastest news from each urls')
 async def _pull_news(ctx):
-    await pull_news(True, ctx)
+    await pull_last_news(ctx)
 
-@bot.command(name="last", help='Fetch and post last news : fetch news')
+@bot.command(name="last", help='Fetch and post lastest news from each urls')
 async def _pull(ctx, arg):
     if arg == "news":
         await _pull_news(ctx)
@@ -183,6 +204,6 @@ async def _status(ctx, arg):
 @_pull_news.error
 @_pull.error
 async def _pull_news_error(ctx, error):
-    logging.exception("Uncaught exception: {0}".format(str(error)))
+    app_log.exception("Uncaught exception: {0}".format(str(error)))
 
 bot.run(DISCORD_TOKEN)
