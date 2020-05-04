b536a702e9a47fe56a298a13bd27453a878feb12
Francois Ancel <francoisancel@gmail.com>
2020-05-04 22:49:37 +0200

    moving periodic pull into a task & rollback on requirement.txt

--- main.py
+++ main.py
@@ -42,14 +42,14 @@ log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(funcName)s(%(line
 file_handler = RotatingFileHandler(LOGGING_PATH, mode='w', maxBytes=5*1024*1024, 
                                  backupCount=2, encoding=None, delay=0)
 file_handler.setFormatter(log_formatter)
-file_handler.setLevel(logging.INFO)
+file_handler.setLevel(logging.DEBUG)
 
 stdout_handler = logging.StreamHandler(sys.stdout)
 stdout_handler.setFormatter(log_formatter)
-stdout_handler.setLevel(logging.INFO)
+stdout_handler.setLevel(logging.DEBUG)
 
 app_log = logging.getLogger('root')
-app_log.setLevel(logging.INFO)
+app_log.setLevel(logging.DEBUG)
 
 app_log.addHandler(file_handler)
 app_log.addHandler(stdout_handler)
@@ -62,9 +62,6 @@ threading.excepthook = exception_handler
 
 def cleanup_summary(summary):
     return BeautifulSoup(summary.replace("<br>", '\n'), "lxml").text
-
-def news_before_date(previous_run, date):
-    return previous_run is None or date is None or previous_run < date
 
 bot = commands.Bot(command_prefix=COMMAND_PREFIX)
 
@@ -170,11 +167,10 @@ async def pull_last_news(ctx=None):
         if len(url_errored) > 0:
                 await local_ctx.send("Sorry these url(s) failed: {0}".format(str(url_errored)))
 
-timer = None
 async def pull_news_at_interval():
     await asyncio.sleep(configuration["interval_second"])
     await pull_news()
-    await pull_news_at_interval()
+    asyncio.create_task(pull_news_at_interval())
 
 @bot.event
 async def on_ready():
@@ -189,24 +185,26 @@ async def on_ready():
         app_log.warning("can't find channel: " + CHANNEL_TARGET)
     await fetch_initial_link()
     app_log.info("initialize link done")
-    await pull_news_at_interval()
-
+    asyncio.create_task(pull_news_at_interval())
+    app_log.info("task launched")
+    
 @bot.command(name='last_news', help='Fetch and post lastest news from each urls')
 async def _pull_news(ctx):
     await pull_last_news(ctx)
 
 @bot.command(name="last", help='Fetch and post lastest news from each urls')
-async def _pull(ctx, arg):
-    if arg == "news":
-        await _pull_news(ctx)
+async def _pull(ctx):
+    await pull_last_news(ctx)
 
 @bot.command(name="status", help='Display current bot status')
-async def _status(ctx, arg):
+async def _status(ctx):
     if "chan_target_id" not in configuration:
        await ctx.send("Sorry, could not find the designated channel to post in")
+    await ctx.send("OK!")
 
 @_pull_news.error
 @_pull.error
+@_status.error
 async def _pull_news_error(ctx, error):
     app_log.exception("Uncaught exception: {0}".format(str(error)))
 
